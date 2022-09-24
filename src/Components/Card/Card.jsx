fbf8b4d391e2660fb239604712718196b6874051
Medha <umredkarmedha11@gmail.com>
2022-09-24 13:59:48 +0530

    initial commit

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import './Card.css'
+
+const Card = (props) => {
+    return(
+        <div className="container">
+              <img className="image" src={props.image} alt="" />
+              <div className="details">
+                <div className="text">
+                  <h1 className="title">{props.name}</h1> 
+                  <h2 className="attacks">Attacks</h2>
+                  {props.attacks && props.attacks.map((item) => <p  style={{display:"inline",fontSize:"12px"}}>{item.name}, </p>)}
+                 
+                  <h2 className="attacks">Abilities</h2>
+                  {props.abilities &&  props.abilities.map((item) => <p  style={{display:"inline",fontSize:"12px"}}>{item.name}</p>)}
+                  
+
+                </div>
+                <div className="hp">
+                <h2 className="attacks">Hp</h2>
+                  {props.hp}
+                </div>
+              </div>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/Components/Pokeman/Pokeman.jsx
@@ -0,0 +1,59 @@
+import './Pokeman.css'
+import Card from "../Card/Card";
+import { useEffect, useState } from 'react';
+
+
+const Pokeman = () => {
+    const [datas, setData] = useState([])
+    const [isFetching, setIsFetching] = useState(false);
+
+    let page = 1500;
+
+    const getPosts = async () => {
+        setIsFetching(true)
+        const response = await fetch(
+          `https://api.pokemontcg.io/v2/cards?page=${page}&pageSize=10`
+        );
+        const data = await response.json();
+        setData([...datas, ...data.data]);
+        setIsFetching(false)
+    };
+
+    function getMorePosts() {
+        setTimeout(() => {
+            page++;
+            getPosts();
+            }, 2000);
+    }
+
+    useEffect(() => {
+        getPosts();
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+      
+    useEffect(() => {
+        if (!isFetching) return;
+        getMorePosts();
+    }, [isFetching]);
+
+    const handleScroll = () => {
+        if (
+          window.innerHeight + document.documentElement.scrollTop !==
+          document.documentElement.offsetHeight
+        )
+          return;
+        setIsFetching(true);
+    }
+
+    return(
+        <div className="pokeman">
+            {Object.keys(datas).map((item) => {
+                return <Card name={datas[item].name} attacks={datas[item].attacks} hp={datas[item].hp} abilities={datas[item].abilities} image={datas[item].images.large} />
+            })}
+        </div>
+    )
+}
+
+export default Pokeman;
