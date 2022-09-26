6500a711b71e4953d9ee56840bb5dc363f405925
Medha <umredkarmedha11@gmail.com>
2022-09-26 15:18:55 +0530

    commit message

--- src/Components/Pokeman/Pokeman.jsx
+++ src/Components/Pokeman/Pokeman.jsx
@@ -7,7 +7,7 @@ const Pokeman = () => {
     const [datas, setData] = useState([])
     const [isFetching, setIsFetching] = useState(false);
     
-    let pageSize = 10;
+    let pageSize = 1;
 
     const getPosts = async () => {
         setIsFetching(true)
